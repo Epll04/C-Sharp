8ea85f239e725ce2af446d9a95d490aca734325e
Epll <epll@yandex.ru>
2023-10-18 13:11:15 +0300

    hw6

---
+++ HomeWork/hw6/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+
+Console.Write("Input M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] Array(int m) // пишем метод для создания массива
+{
+    int[] array = new int[m]; //тело метода - при создании массива, такая конструкция выделяет память для аргумента size в компьютере для массива целых чисел размером size
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine($"Введите {i+1} элемент массива ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray (int[] array) //метод PrintArray - чтобы вывести на экран наш массив
+{
+    for (int i=0; i < array.Length; i++)
+    {
+        System.Console.Write(array[i] + " ");
+    }
+System.Console.WriteLine();
+}
+
+int CountN(int[] Array)
+{
+    int count = 0;
+    for (int i = 1; i<Array.Length; i = i+1)
+    {
+        if(i > 0) 
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+int[] myArray = Array(m);
+PrintArray(myArray);
+
+System.Console.Write(CountN);
+
+
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1,
+// y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+
---
+++ Seminar/Sem6/Program.cs
@@ -0,0 +1,163 @@
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный
+// массив (последний элемент будет на первом месте, а первый - на
+// последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+
+
+// int[] CreateRandomArray(int size, int minValue, int maxValue)  // пишем метод для создания массива
+// {
+//     int[] array = new int[size]; //тело метода - при создании массива, такая конструкция
+//                                 // выделяет память для аргумента size в компьютере для массива целых чисел размером size
+//     for (int i = 0; i < size; i++) // Чтобы пройтись по массиву обратиться к каждому элементу.
+//                                       //и когда речь идет о массиве всегда цикл For
+//     {
+//         array[i] = new Random().Next(minValue, maxValue + 1); // заполняем массив
+//     }
+//     return array;
+// }
+
+// void PrintArray (int[] array) //метод PrintArray - чтобы вывести на экран наш массив
+// {
+//     for (int i=0; i < array.Length; i++)
+//     {
+//         System.Console.Write(array[i] + " ");
+//     }
+//     System.Console.WriteLine();
+// }
+
+// int[] ReverseArray(int[] array)   // Метод для решения задачи
+// {
+//     int temp;
+//     for (int i = 0; i< array.Length/2; i++)
+//     {
+//         temp = array[i];
+//         array[i] = array[array.Length - 1 - i];
+//         array[array.Length - 1 - i] = temp;
+//     }
+//     return array;
+// }
+
+// System.Console.WriteLine("Input array size: ");
+// int size = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input minimal value: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine("Input maximal value: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+
+// int[] myArray = CreateRandomArray(size, min, max); //Печатаем начальный массив 
+// PrintArray(myArray);
+// myArray = ReverseArray(myArray); //Печатаем переделанный массив
+// PrintArray(myArray);
+
+// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+
+// string DecimalToBinary(int num) //метод решения задачи
+// {
+//     string result = string.Empty;
+//     while (num > 0)
+//     {
+//        result = num % 2 + result;
+//        num /= 2;
+//     }
+//     return result;
+// }
+
+
+// System.Console.WriteLine("Input num");
+// int num = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine (DecimalToBinary(num));
+
+
+// Задача 40: Напишите программу, которая принимает на вход три числа и
+// проверяет, может ли существовать треугольник с сторонами такой длины.
+// Теорема о неравенстве треугольника: каждая сторона треугольника
+// меньше суммы двух других сторон.
+
+// System.Console.Write("Enter first number: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Enter second number: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// System.Console.Write("Enter third number: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// bool Triangle (int number1, int number2, int number3)
+// {
+//     if((number1<(number2+number3)) && (number2<(number1+number3)) && (number3<(number2+number1)))
+//     {
+//         return true;
+//     }
+//     else
+//      {
+//         return false;
+//     }
+// }
+// System.Console.WriteLine (Triangle (number1, number2, number3));
+
+
+
+// Задача 44: Не используя рекурсию, выведите первые N чисел
+// Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+// Если N = 5 -> 0 1 1 2 3
+// Если N = 3 -> 0 1 1
+// Если N = 7 -> 0 1 1 2 3 5 8
+using System;
+
+
+    static void Main(string[] args)
+    {
+        Console.Write("Input N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int[] fibonachi = new int [N];
+        Console.Write($"Если N = {N} ->" );
+        PrintArray(Fibonachi(fibonachi));
+    }
+
+    static int[] Fibonachi(int[] fibonachi)
+    {
+        fibonachi[0] = 0;
+        fibonachi[1] = 1;
+        for(int i = 2; i < fibonachi.Length; i++)
+        {
+            fibonachi[i] = fibonachi[i-1] + fibonachi[i-2];
+        }
+        return fibonachi;
+    }
+
+    void PrintArray(int[] array)
+    {
+        for(int i = 0; i < array.Length; i++)
+        {
+            if (i == 0)
+            {
+                Console.Write($"[{array[i]}], ");
+            }
+            else if (i == array.Length-1)
+            {
+            Console.Write($"[{array[i]}], ");
+            }
+            else
+            {
+            Console.Write($"[{array[i]}], ");
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
+// Задача 45: Напишите программу, которая будет создавать копию
+// заданного массива с помощью поэлементного копирования.
+
+
